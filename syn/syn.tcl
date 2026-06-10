18216402383a30b94e467fbb65739218bc1f5841
nickocruzm <nickocruzm@gmail.com>
2026-06-10 15:38:03 -0700

    sobel added

--- syn/syn.tcl
+++ syn/syn.tcl
@@ -25,7 +25,7 @@ set mw_logic0_net "VSS"
 set DESIGN_NAME     "conv"
 
 # Analyze the Verilog source files
-analyze -format verilog "conv.v mac.v register.v shift.v"
+analyze -format verilog "sobel.v conv.v mac.v register.v shift.v"
 
 # Elaborate the design
 elaborate ${DESIGN_NAME} -architecture verilog -library DEFAULT
